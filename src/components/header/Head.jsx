c99e6279c93ee5a637bac0c648179dfb51a36ac7
Arun Surendran <arunid.surya@gmail.com>
2023-06-01 21:36:03 +0200

    Header created

---
+++ src/components/header/Head.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const Head = () => {
+  return (
+    <>
+      <section className='head' >
+        <div className="container flexSB PaddingTB">
+            <div className="logo">
+                <img src="../images/logo.png" alt="" />
+            </div>
+            <div className="ad">
+                <img src="../images/headerb.png" alt="" />
+            </div>
+        </div>
+
+      </section>
+    </>
+  )
+}
+
+export default Head
